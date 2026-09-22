1921f8048f68b556e1e66fbfa2a61868ccbb416e
kanta <h.kantateen48690415@gmail.com>
2026-09-22 13:32:26 +0900

    検索機能細分化

--- frontend/script.js
+++ frontend/script.js
@@ -14,11 +14,12 @@ new Splide('.splide',{
 //参考書検索機能
 const btn = document.getElementById("button");
 const searchInput = document.getElementById("search");
+ const parts = document.querySelectorAll(".reference-part");
 
 btn.onclick = function () {
     const search = searchInput.value;
 
-    const parts = document.querySelectorAll(".reference-part");
+   
 
     parts.forEach((part) => {
         const book = part.querySelector(".reference-name");
@@ -30,10 +31,10 @@ btn.onclick = function () {
         }
     });
 };
-
+//著者フィルター
 const author = document.getElementById("Sort")
 author.addEventListener('change', (e) => {
-  const parts = document.querySelectorAll(".reference-part");
+ 
   const selectValue  = e.target.value;
   if(selectValue === "1"){
     parts.forEach((part) => { 
@@ -42,7 +43,69 @@ author.addEventListener('change', (e) => {
         } else {
             part.classList.add("hidden")
         }
-    });
+      });
+      hamreset();
+      authorreset();
   }
 
 });
+
+ function authorreset(){
+   const authorReset = document.getElementById("Sort");
+   authorReset.value='';
+ }
+//難易度フィルター
+const level = document.getElementById("level-filter")
+level.addEventListener('change',(e)=>{
+  
+  const selectValue1 = e.target.value;
+  if(selectValue1 === "1"){
+    parts.forEach((part) =>{
+      if(part.dataset.level === "基礎"){
+        part.classList.remove("hidden")
+      }
+      
+      else{
+        part.classList.add("hidden")
+      }
+     
+    })
+  }
+if(selectValue1 === "3"){
+  parts.forEach((part) =>{
+    if(part.dataset.level ==="発展"){
+      part.classList.remove("hidden")
+    }
+    else{
+      part.classList.add("hidden")
+    }
+  })
+}
+})
+
+
+//検索結果リセット
+const resetbutton = document.getElementById("reset");
+resetbutton.addEventListener('click',(e)=>{
+   parts.forEach((part)=>{
+    part.classList.remove("hidden")
+   })
+  hamreset();
+})
+
+
+
+
+//ハンバーガーメニュー
+const ham = document.getElementById("hamburger");
+const hambutton = document.getElementById("hamburger-button")
+
+hambutton.addEventListener('click',function(){
+  ham.classList.toggle('active');
+  hambutton.classList.toggle('active');
+})
+
+function hamreset (){
+  ham.classList.remove('active')
+  hambutton.classList.remove('active')
+}
